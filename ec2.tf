19c7a1d5a5e1e6deff7ef2b665d9dec2be1e48f6
sunnyphulphagar <sunnyphulphagar@gmail.com>
2025-08-23 00:17:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mytfinstance"{
         ami = "ami-0cbbe2c6a1bb2ad63"
         instance_type = "t2.micro"
-        tags = { Name = "myinstance"}
+        tags = { Name = "myinstance1"}
 }
